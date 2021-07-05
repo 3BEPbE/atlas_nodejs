2cb432fb10549ab5eeee940ab81627c3211f4539
Nick Simmons <nicksimmons@MacBook-Pro-Nick-2.local>
2021-07-05 10:52:43 +0500

    added mongoose and env connection files

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
-// Routes files
-const bootcamps = require('./routes/bootcamps');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
+
+// Connect to database
+connectDB();
+
+// Routes files
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
@@ -23,8 +27,14 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 8000;
 
-app.listen(
+const server = app.listen(
     PORT, 
     console.log(`Server running on ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
 
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    // Close server and exit process
+    server.close(() => process.exit(1));
+})
